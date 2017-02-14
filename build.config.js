a02fe1f4e2d5a823176faec7ccc45029d5d58808
Gennady Verbitsky <gennady.verbitsky@minsk.finfactory.com>
2017-02-14 15:51:53 +0300

    buildspec + dependencies fix

--- build.config.js
+++ build.config.js
@@ -1,6 +1,6 @@
 const config = {
   PORT: 8888,
-  PROXY_TARGET: 'localhost',                          // domain
+  PROXY_TARGET: 'https://baihbjchk3.execute-api.eu-west-1.amazonaws.com',                          // domain
   APP_NAME: 'cashflow',                               // name of the app
   APP_NAME_SHORT: 'CFMT',                             // 2 letter shortname
   APP_COMPANY: 'Finfactory',                          // name of company
